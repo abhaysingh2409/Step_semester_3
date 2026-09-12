cbec9a082a7753377700ffa38e6921d319010b95
Abhay Pratap Singh <singhpratapabhay2006@gmail.com>
2026-09-12 23:03:25 +0530

    feat(session_3): implement 5 class problems for week 3 oop fundamental

---
+++ src/main/java/oop_fundamental/class_problems/Student.java
@@ -0,0 +1,14 @@
+package oop_fundamental.class_problems;
+
+public class Student {
+    String name;
+    int marks;
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Divya";
+        student.marks = 88;
+
+        System.out.println("Name: " + student.name + " | Marks: " + student.marks);
+    }
+}
